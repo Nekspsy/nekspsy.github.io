fb3725e6153e3b81751aaa4ade52810f6a9d10d2
Nekspsy <neks.ps@gmail.com>
2017-08-01 21:33:34 +0300

    3

--- insta/js/common.js
+++ insta/js/common.js
@@ -1,5 +1,27 @@
 $(function() {
 
+	$('.slider-wrap').slideDown();
+
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+
+	$("a[href=#callback]").click(function() {
+		$("#callback .formname").val($(this).data("form"));
+	});
+
 	$(".service-item h4").equalHeights();
 	$(".new-item-text").equalHeights();
 	$(".link-item").equalHeights();
@@ -26,29 +48,54 @@ $(function() {
 	});
 
 	$(".sf-menu").after("<div id='my-menu'>");
-	$(".sf-menu").clone().appendTo("#my-menu");
-	$("#my-menu").find("*").attr("style", "");
-	$("#my-menu").find("ul").removeClass("sf-menu");
-	$("#my-menu").mmenu({
-		extensions : [ 'theme-white', 'effect-menu-slide', 'pagedim-black'/*, 'widescreen'*/ ],
-		navbar: {
-			title: "Меню"
-		}
-	});
+		$(".sf-menu").clone().appendTo("#my-menu");
+		$("#my-menu").find("*").attr("style", "");
+		$("#my-menu").find("ul").removeClass("sf-menu");
+		$("#my-menu").mmenu({
+			extensions : [ /*'widescreen',*/ 'theme-white', 'effect-menu-slide', 'pagedim-black' ],
+			navbar: {
+				title: "Меню"
+			}
+		});
 
-	var api = $("#my-menu").data("mmenu");
-	api.bind("closed", function() {
-		$(".toggle-mnu".removeClass("on"));
-	});
+		var api = $("#my-menu").data("mmenu");
+		api.bind("closed", function () {
+			$(".toggle-mnu").removeClass("on");
+		});
 
-	$(".mobile-mnu").click(function() {
-		var mmAPI = $("#my-menu").data( "mmenu" );
-		mmAPI.open();
-		var thiss = $(this).find(".toggle-mnu");
-		thiss.toggleClass("on");
-		$(".main-mnu").slideToggle();
-		return false;
-	});
+		$(".mobile-mnu").click(function() {
+			var mmAPI = $("#my-menu").data( "mmenu" );
+			mmAPI.open();
+			var thiss = $(this).find(".toggle-mnu");
+			thiss.toggleClass("on");
+			$(".main-mnu").slideToggle();
+			return false;
+		});
+
+	// $(".sf-menu").after("<div id='my-menu'>");
+	// $(".sf-menu").clone().appendTo("#my-menu");
+	// $("#my-menu").find("*").attr("style", "");
+	// $("#my-menu").find("ul").removeClass("sf-menu");
+	// $("#my-menu").mmenu({
+	// 	extensions : [ 'theme-white', 'effect-menu-slide', 'pagedim-black'/*, 'widescreen'*/ ],
+	// 	navbar: {
+	// 		title: "Меню"
+	// 	}
+	// });
+	//
+	// var api = $("#my-menu").data("mmenu");
+	// api.bind("closed", function() {
+	// 	$(".toggle-mnu".removeClass("on"));
+	// });
+	//
+	// $(".mobile-mnu").click(function() {
+	// 	var mmAPI = $("#my-menu").data( "mmenu" );
+	// 	mmAPI.open();
+	// 	var thiss = $(this).find(".toggle-mnu");
+	// 	thiss.toggleClass("on");
+	// 	$(".main-mnu").slideToggle();
+	// 	return false;
+	// });
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -59,18 +106,20 @@ $(function() {
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
+	$("callback").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+			$(".success").addClass("visible");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
-			}, 1000);
+				$(".success").removeClass("visible");
+				$.magnificPopup.close();
+			}, 3000);
 		});
 		return false;
 	});
