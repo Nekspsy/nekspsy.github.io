e48112f7a876f58f1d6838852cb496d86fa589d4
Nekspsy <neks.ps@gmail.com>
2017-08-02 16:43:34 +0300

    3

--- yurdis/js/common.js
+++ yurdis/js/common.js
@@ -124,4 +124,18 @@ $(function() {
 		return false;
 	});
 
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	$(".preloader").fadeOut();
+
 });
