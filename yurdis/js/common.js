33b4ff49130f0419530d9841fd2b917ecd8f75bb
Nekspsy <neks.ps@gmail.com>
2017-08-02 18:19:56 +0300

    9

--- yurdis/js/common.js
+++ yurdis/js/common.js
@@ -72,31 +72,6 @@ $(function() {
 			return false;
 		});
 
-	// $(".sf-menu").after("<div id='my-menu'>");
-	// $(".sf-menu").clone().appendTo("#my-menu");
-	// $("#my-menu").find("*").attr("style", "");
-	// $("#my-menu").find("ul").removeClass("sf-menu");
-	// $("#my-menu").mmenu({
-	// 	extensions : [ 'theme-white', 'effect-menu-slide', 'pagedim-black'/*, 'widescreen'*/ ],
-	// 	navbar: {
-	// 		title: "Меню"
-	// 	}
-	// });
-	//
-	// var api = $("#my-menu").data("mmenu");
-	// api.bind("closed", function() {
-	// 	$(".toggle-mnu".removeClass("on"));
-	// });
-	//
-	// $(".mobile-mnu").click(function() {
-	// 	var mmAPI = $("#my-menu").data( "mmenu" );
-	// 	mmAPI.open();
-	// 	var thiss = $(this).find(".toggle-mnu");
-	// 	thiss.toggleClass("on");
-	// 	$(".main-mnu").slideToggle();
-	// 	return false;
-	// });
-
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
