6abe7fac213aa15f8d1bcd3d57ddb8d0cda75a3b
Neks <neks.ps@gmil.com>
2019-02-03 18:14:37 +0300

    3.02 c

--- portfolio/S&Mitler/gulpfile.js
+++ portfolio/S&Mitler/gulpfile.js
@@ -1,7 +1,7 @@
 var syntax        = 'sass', // Syntax: sass or scss;
 		gulpversion   = '4'; // Gulp version: 3 or 4
 
-var gulp          = requirez
+var gulp          = require('gulp'),
 		gutil         = require('gulp-util' ),
 		sass          = require('gulp-sass'),
 		browserSync   = require('browser-sync'),
@@ -38,6 +38,7 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/mmenu/dist/jquery.mmenu.all.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
